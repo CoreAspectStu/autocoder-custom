240127f433b005473b9a519592c951d1d6b42e22
Stuart <stuart@coreaspect.com>
2026-01-25 08:17:55 +1100

    Fix analytics router export in routers/__init__.py
    Add analytics_router to imports and __all__ list to fix ImportError on startup

--- server/routers/__init__.py
+++ server/routers/__init__.py
@@ -6,6 +6,7 @@ FastAPI routers for different API endpoints.
 """
 
 from .agent import router as agent_router
+from .analytics import router as analytics_router
 from .assistant_chat import router as assistant_chat_router
 from .devserver import router as devserver_router
 from .expand_project import router as expand_project_router
@@ -24,6 +25,7 @@ __all__ = [
     "projects_router",
     "features_router",
     "agent_router",
+    "analytics_router",
     "schedules_router",
     "devserver_router",
     "spec_creation_router",
